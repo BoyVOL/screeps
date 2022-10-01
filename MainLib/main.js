c2e1f514c83cebd4f3ec08bae03f4f9551215027
BoyVOL <39579095+BoyVOL@users.noreply.github.com>
2022-10-01 18:48:43 +0900

    1

--- MainLib/main.js
+++ MainLib/main.js
@@ -5,9 +5,6 @@ const { MemoryItem } = require('./MemoryManagement');
 console.log("______________________________________________________________________________");
 
 function loop(){
-    var item = new MemoryItem("bla","",Memory.creeps);
-    item.value += "bla";
-    console.log(item.value);
 
     console.log(spawnTable.objArray.length);
     spawnTable.Update();
