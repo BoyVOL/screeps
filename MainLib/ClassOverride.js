927a1d0802d2a55a6e2f1326f21d8ad3093d1853
BoyVOL <vladislav_limonov@mail.ru>
2022-10-03 17:53:37 +0900

    1

--- MainLib/ClassOverride.js
+++ MainLib/ClassOverride.js
@@ -100,7 +100,7 @@ class ObjTable extends HtableOverride{
             if(!pass.ObjExists(key)){
                 pass.AddObject(val,key);
             }
-            pass.objArray[key].LoadOrig(pass.orig[key]);
+            pass.objArray[key].LoadOrig(val);
         });
         this.forEachObj(function(val,key){
             if(!pass.Exists(key)){
