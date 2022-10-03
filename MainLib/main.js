a429c563c26d74e0ef387dead173fc4b892ddc7a
BoyVOL <vladislav_limonov@mail.ru>
2022-10-03 14:39:58 +0900

    1

--- MainLib/main.js
+++ MainLib/main.js
@@ -1,9 +1,9 @@
 const { RoomOverride,RoomsTable,roomTable} = require('./RoomsOverride');
 const { SpawnOverride,SpawnTable,spawnTable} = require('./SpawnOverride');
+console.log("______________________________________________________________________________");
 const { CreepOverride,CreepTable,creepTable} = require('./CreepOverride');
 const { StructureOverride,StructureTable,structTable} = require("./StructureOverride");
 const { MemoryItem } = require('./MemoryManagement');
-console.log("______________________________________________________________________________");
 
 
 function loop(){
