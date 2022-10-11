139f1d1baddffb738a8e25516d36fdc5e1a1c826
BoyVOL <vladislav_limonov@mail.ru>
2022-10-11 14:49:03 +0900

    1

--- MainLib/MoveControl.js
+++ MainLib/MoveControl.js
@@ -7,7 +7,7 @@ class Movement extends WithParent{
         super(parent);
         this.path = new MemoryItem("path",new Array(),this.parent.orig.memory);
         this.lastresult = 0;
-        this.target = null;
+        this.target = new MemoryItem("target",null,this.parent.orig.memory);
     }
 
     DrawPath(){
@@ -16,8 +16,8 @@ class Movement extends WithParent{
     }
 
     DrawTarget(){
-        if(this.target != null){
-        this.parent.Room.orig.visual.circle(this.target,
+        if(this.target.value != null){
+        this.parent.Room.orig.visual.circle(this.target.value,
             {fill: 'transparent', radius: 0.55, stroke: 'red'});
         }
     }
@@ -51,8 +51,8 @@ class Movement extends WithParent{
     }
 
     GetNewPath(range = 0){
-        if(this.target!=null){
-            this.path.value = this.parent.orig.pos.findPathTo(this.target,{range: range});
+        if(this.target.value!=null){
+            this.path.value = this.parent.orig.pos.findPathTo(this.target.value,{range: range});
         }
     }
 
@@ -78,6 +78,7 @@ class Movement extends WithParent{
     Update(){
         super.Update();
         this.path = new MemoryItem("path",new Array(),this.parent.orig.memory);
+        this.target = new MemoryItem("target",null,this.parent.orig.memory);
         console.log(this.IsOnPath,this.MovIsGood,this.PathIsComplete,this.Tired);
         this.CheckArrival();
         this.CheckStayingOnPath();
