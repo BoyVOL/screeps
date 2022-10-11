270abfe761239b73a716e3766f6134cff39a71f7
BoyVOL <vladislav_limonov@mail.ru>
2022-10-11 15:13:52 +0900

    1

--- MainLib/MoveControl.js
+++ MainLib/MoveControl.js
@@ -28,7 +28,6 @@ class Movement extends WithParent{
 
     get PathIsToTarget(){
         if(this.target != null && this.PathIsDefined){
-            console.log(this.path.value.length);
             return this.path.value[this.path.value.length-1].x == this.target.x,
             this.path.value[this.path.value.length-1].y == this.target.y;
         } else return false;
