0530f0ba64d9ab2144dc37602bfc8e4e52fda489
BoyVOL <vladislav_limonov@mail.ru>
2022-10-13 15:44:25 +0900

    11

--- MainLib/TaskManager.js
+++ MainLib/TaskManager.js
@@ -1,4 +1,4 @@
-const { HtableOverride } = require('./ClassOverride');
+const { HtableOverride,plainTable,WithParent } = require('./ClassOverride');
 const { MemoryItem } = require('./MemoryManagement');
 
 class TaskTable extends HtableOverride{
@@ -9,10 +9,43 @@ class TaskTable extends HtableOverride{
     LoadOrig(){
         this.orig = new MemoryItem('taskTable',{}).value;
     }
+
+    UploadTask(task){
+        this.AddRecord(task.id,task.data);
+    }
+}
+
+class Task{
+    constructor(data){
+        if(typeof(data.id) != 'undefined'){  
+            this.id = data.id;
+        } else {
+            this.id = Date.now();
+        }
+        this.type = data.type;
+        this.parentid = data.parentid;
+    }
+
+    get parent(){
+        return plainTable.objects[this.parentid];
+    }
+
+    get data(){
+        return {
+            id: this.id,
+            type : this.type,
+            parentid: this.parentid
+        }
+    }
+}
+
+class TaskProvider extends WithParent{
+    
 }
 
 const taskTable = new TaskTable();
 
 module.exports = {
+    Task,
     taskTable
 }
