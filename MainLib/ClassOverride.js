665975b5817bd7391c8179a1bc9b7aa2d7c92916
BoyVOL <vladislav_limonov@mail.ru>
2022-10-13 10:46:39 +0900

    Revert "1"
    This reverts commit e628f00748ed09c716e62a9d2dd12dc6e786bc1a.

--- MainLib/ClassOverride.js
+++ MainLib/ClassOverride.js
@@ -162,6 +162,7 @@ class ObjProxyTable extends HtableOverride{
     /** cycle for all items in table that gets function as a parameter */
     forEachObj(funct){
         for (const key in this.objArray) {
+            console.log(key);
             funct(this.objArray[key],key);
         }
     }
@@ -172,7 +173,6 @@ class ObjProxyTable extends HtableOverride{
 
         var funct = function(obj,key){
             obj.Update();
-            console.log(key);
         }
         this.forEachObj(funct);
     }
