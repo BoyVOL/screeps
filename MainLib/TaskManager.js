021064e4455537b6fb3ed4de7d11e8d617d0ae68
BoyVOL <vladislav_limonov@mail.ru>
2022-10-14 11:15:14 +0900

    1

--- MainLib/TaskManager.js
+++ MainLib/TaskManager.js
@@ -102,7 +102,7 @@ class TaskExecuter extends WithParent{
 
     OccupyTask(taskId){
         this.activeTaskId.value = taskId;
-        console.log(taskTable.orig[taskId].executerid, " executre");
+        console.log(taskTable.orig[taskId].id,taskTable.orig[taskId].executerid, " executre");
         taskTable.orig[taskId].executerid = this.parent.orig.key;
     }
 
