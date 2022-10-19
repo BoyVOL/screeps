06e0329228fb1e6e59dd415dae6877f3546f7435
BoyVOL <vladislav_limonov@mail.ru>
2022-10-19 17:35:54 +0900

    1

--- MainLib/CreepOverride.js
+++ MainLib/CreepOverride.js
@@ -15,7 +15,6 @@ class CreepOverride extends RoomObjectOver{
         super.Update();
         this.taskExecuter.Update();
         if(this.taskExecuter.activeTask != null) {
-            var dest = 
             this.MoveContr.target = this.taskExecuter.activeTask.dest;
         }
         this.MoveContr.Update();
