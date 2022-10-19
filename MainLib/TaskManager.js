f1fa1f0d7e477bb87ae065c99465a45244fce438
BoyVOL <vladislav_limonov@mail.ru>
2022-10-19 17:11:15 +0900

    1

--- MainLib/TaskManager.js
+++ MainLib/TaskManager.js
@@ -104,7 +104,7 @@ class TaskExecuter extends TaskHandler{
 
     Update(){
         super.Update();
-        console.log(this.GetTask(this.PickRandomTaskID()).id);
+        console.log(this.GetTask(this.PickRandomTaskID()).orig.id);
         this.activeTaskId = new MemoryItem('activeTaskId',null,this.parent.orig.memory);
     }
 }
