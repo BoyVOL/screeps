0deca48537a8a0702f4ada17a2586f0ae4a778aa
BoyVOL <vladislav_limonov@mail.ru>
2022-10-25 16:47:31 +0900

    1

---
+++ MainLib/ActionControl.js
@@ -0,0 +1,50 @@
+const { MemoryItem } = require('./MemoryManagement');
+const { ObjectOverride,ObjProxyTable } = require('./ClassOverride');
+
+class ActionTable extends ObjProxyTable{
+
+    constructor(actor){
+        super(new MemoryItem('taskTable',{},actor.orig.memory).value);
+        this.actor = actor;
+    }
+    
+    LoadOrig(){
+        this.orig = new MemoryItem('taskTable',{},actor.orig.memory).value;
+    }
+
+    InitSingleObject(orig){
+        switch (orig.type) {
+            case 'mov':
+                    return new MovAction(orig,this.actor);
+                break;
+        
+            default:
+                    return new Action(orig,this.actor);
+                break;
+        }
+    }
+
+    Update(){
+        super.Update();
+        console.log("BRUH");
+    }
+}
+
+class Action extends ObjectOverride{
+    constructor(orig,actor){
+        super(orig);
+        this.actor = actor;
+    }
+}
+
+class MovAction extends Action{
+    constructor(orig,actor){
+        super(orig,actor);
+    }
+}
+
+module.exports = {
+    ActionTable,
+    Action,
+    MovAction,
+}
--- MainLib/CreepOverride.js
+++ MainLib/CreepOverride.js
@@ -1,14 +1,22 @@
 const { ObjProxyTable } = require('./ClassOverride');
+const { ActionTable } = require('./ActionControl');
 const { RoomObjectOver } = require('./RoomsOverride');
 
 class CreepOverride extends RoomObjectOver{
 
     constructor(orig){
         super(orig);
+        this.actionTable = new ActionTable(this);
+    }
+
+    LoadOrig(){
+        super.LoadOrig();
+        this.actionTable.LoadOrig();
     }
 
     Update(){
         super.Update();
+        this.actionTable.Update();
     }
 }
 
