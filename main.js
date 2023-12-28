8876af10b3aa2aa19042edc35cb9e72b4897a353
BoyVOL <vladislav_limonov@mail.ru>
2023-12-28 11:56:01 +0900

    Финальный коммит

--- main.js
+++ main.js
@@ -1,26 +1,6 @@
 console.log("______________________________________________________________________________");
-const { roomTable} = require('./RoomsOverride');
-const { spawnTable} = require('./SpawnOverride');
-const { creepTable} = require('./CreepOverride');
-const { structTable} = require("./StructureOverride");
-const { MemoryItem } = require('./MemoryManagement');
-const { flagTable } = require('./FlagsOverride');
-const { plainTable } = require('./ClassOverride');
-const {createUUID} = require('./UUID');
 
 function loop(){
-    console.log("UPDATE >>>>>>>>");
-    roomTable.UpdateObjects();
-    flagTable.UpdateObjects();
-    structTable.UpdateObjects();
-    spawnTable.UpdateObjects();
-    creepTable.UpdateObjects();
-
-    roomTable.Update();
-    flagTable.Update();
-    structTable.Update();
-    spawnTable.Update();
-    creepTable.Update();
 }
 
 module.exports = {
