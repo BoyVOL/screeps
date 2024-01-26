f55382753e9528348e174860e6895d1f5879b63b
BoyVOL <vladislav_limonov@mail.ru>
2024-01-26 12:48:15 +0900

    1

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 console.log("______________________________________________________________________________");
 
 const {RoomTable} = require('./RoomOverride');
+const {plainTable} = require('./PlainClassTable');
 
 var RTable = new RoomTable;
 
@@ -8,7 +9,8 @@ function loop(){
     RTable.Update();
 
     console.log(RTable.AsArray());
-    console.log(RTable.objects['sim1']);
+    console.log(RTable.objects['sim'].key);
+    console.log(plainTable.objects['sim'].key);
 }
 module.exports = {
     loop,
